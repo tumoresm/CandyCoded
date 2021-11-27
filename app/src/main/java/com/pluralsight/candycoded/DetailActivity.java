8695df3710c41f039d3a8e621defb88d9578780d
Tumo <tumoresm@gmail.com>
2021-11-27 16:14:47 +0200

    Plural project

---
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -0,0 +1,103 @@
+package com.pluralsight.candycoded;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.pluralsight.candycoded.DB.CandyContract;
+import com.pluralsight.candycoded.DB.CandyContract.CandyEntry;
+import com.pluralsight.candycoded.DB.CandyDbHelper;
+import com.squareup.picasso.Picasso;
+
+public class DetailActivity extends AppCompatActivity {
+
+    public static final String SHARE_DESCRIPTION = "Look at this delicious candy from Candy Coded - ";
+    public static final String HASHTAG_CANDYCODED = " #candycoded";
+    String mCandyImageUrl = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent intent = DetailActivity.this.getIntent();
+
+        if (intent != null && intent.hasExtra("position")) {
+            int position = intent.getIntExtra("position", 0);
+
+            CandyDbHelper dbHelper = new CandyDbHelper(this);
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            Cursor cursor = db.rawQuery("SELECT * FROM candy", null);
+            cursor.moveToPosition(position);
+
+            String candyName = cursor.getString(cursor.getColumnIndexOrThrow(
+                    CandyContract.CandyEntry.COLUMN_NAME_NAME));
+            String candyPrice = cursor.getString(cursor.getColumnIndexOrThrow(
+                    CandyEntry.COLUMN_NAME_PRICE));
+            mCandyImageUrl = cursor.getString(cursor.getColumnIndexOrThrow(
+                    CandyEntry.COLUMN_NAME_IMAGE));
+            String candyDesc = cursor.getString(cursor.getColumnIndexOrThrow(
+                    CandyEntry.COLUMN_NAME_DESC));
+
+
+            TextView textView = (TextView) this.findViewById(R.id.text_view_name);
+            textView.setText(candyName);
+
+            TextView textViewPrice = (TextView) this.findViewById(R.id.text_view_price);
+            textViewPrice.setText(candyPrice);
+
+            TextView textViewDesc = (TextView) this.findViewById(R.id.text_view_desc);
+            textViewDesc.setText(candyDesc);
+
+            ImageView imageView = (ImageView) this.findViewById(
+                    R.id.image_view_candy);
+            Picasso.with(this).load(mCandyImageUrl).into(imageView);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    /**
+     * This hook is called whenever an item in your options menu is selected.
+     * The default implementation simply returns false to have the normal
+     * processing happen (calling the item's Runnable or sending a message to
+     * its Handler as appropriate).  You can use this method for any items
+     * for which you would like to do processing without those other
+     * facilities.
+     *
+     * <p>Derived classes should call through to the base class for it to
+     * perform the default menu handling.</p>
+     *
+     * @param item The menu item that was selected.
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     * @see #onCreateOptionsMenu
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        createShareIntent();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND)
+                .setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION
+                        + mCandyImageUrl + HASHTAG_CANDYCODED);
+        startActivity(shareIntent);
+
+    }
+}
---
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -0,0 +1,53 @@
+package com.pluralsight.candycoded;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
+import android.net.Uri;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
+        ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
+        Picasso.with(this).
+                load(uri).
+                into(candyStoreImageView);
+
+
+    }
+
+   public void createMapIntent(View view){
+        Uri.parse("geo:0.0?q=618 E South St Orlando, FL 32801");
+
+       Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0.0?q=618 E South St Orlando, FL 32801"))
+       .setPackage("com.google.android.apps.maps");
+
+       if (mapIntent.resolveActivity(getPackageManager()) != null){
+                   startActivity(mapIntent);
+       }
+
+   }
+
+   public void createPhoneIntent(View view){
+       Uri tel = Uri.parse("0123456789");
+
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, tel);
+        startActivity(callIntent);
+   }
+
+}
